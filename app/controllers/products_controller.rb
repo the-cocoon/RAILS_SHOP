a3093b4dc1f2dee539b1044d892b5946fcaed101
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-02-06 19:17:00 +0300

    ~~~

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,51 @@
+class FixedPagination
+  def initialize(conn, params)
+    @conn  = conn
+    @page  = params[:page] || 1
+    @limit = 24
+  end
+
+  def current_page
+    @page.to_i
+  end
+
+  # Manual calculation of pages
+  def total_pages
+    sql = <<-EOS.squish
+      SELECT
+         COUNT(RES.*)
+      FROM
+         (
+            SELECT
+               shop_category_rels.item_id   AS item_id,
+               shop_category_rels.item_type AS item_type
+            FROM
+               "shop_category_rels"
+            WHERE
+               (
+                  "shop_category_rels"."item_amount" != 0
+               )
+               AND "shop_category_rels"."item_state" = 'published'
+            GROUP BY
+               item_id,
+               item_type
+         ) AS RES
+      ;
+    EOS
+
+    records = @conn.execute(sql).getvalue(0,0)
+    (records.to_f / @limit.to_f).ceil
+  end
+
+  def limit_value
+    @limit
+  end
+
+  def offset
+    (@page - 1) * @limit
+  end
+end
+
 class ProductsController < RailsShopController
   layout 'rails_shop_layout'
 
@@ -27,6 +75,7 @@ class ProductsController < RailsShopController
         .pagination(params)
 
     @shop_items = @shop_category_rels.map(&:item)
+    @shop_category_rels = FixedPagination.new(ActiveRecord::Base.connection, params)
   end
 
   def show; end
