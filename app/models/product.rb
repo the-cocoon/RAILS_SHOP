5ef852dfd8da8fefa446909be7d1adbf38b52eba
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-04-10 19:41:27 +0300

    ~~~

--- app/models/product.rb
+++ app/models/product.rb
@@ -65,11 +65,58 @@ class Product < ActiveRecord::Base
 
   # SERVICE METHODS
 
+  # ================================================
+  # PARAMS CARD
+  # ================================================
+
   def params_card
     shop_params_card
   end
 
+  def params_card_full_list
+    return nil unless params_card
+
+    card_klass = params_card.class
+    full_list = card_klass.card_fields
+
+    return nil unless full_list
+
+    res = full_list.map do |field_name, field_params|
+      field_name if params_card.send(field_name).present?
+    end.compact
+
+    res.present? ? res : nil
+  end
+
+  def params_card_short_list
+    return nil unless params_card
+
+    card_klass = params_card.class
+    short_list = card_klass.params_short_list
+
+    return nil unless short_list
+
+    res = short_list.map do |field_name, field_params|
+      field_name if params_card.send(field_name).present?
+    end.compact
+
+    res.present? ? res : nil
+  end
+
+  def short_list_params_for(field_name)
+    params_card.class.params_short_list[field_name]
+  end
+
+  def full_list_params_for(field_name)
+    params_card.class.card_fields[field_name]
+  end
+
+  # ================================================
+  # SEARCH HELPERS
+  # ================================================
+
   # Product.first.fts_data_rebuild!
+  # FULL TEXT SEARCH by title
   def fts_data_rebuild!
     fts_data = shop_categories.map(&:title) | shop_brands.map(&:title) | [title]
     fts_data = fts_data.join ', '
