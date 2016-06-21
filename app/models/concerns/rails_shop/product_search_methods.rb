c16e35147124dfae5eb1f59c5c417a1b4ff0a96f
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-06-21 22:59:18 +0300

    ~~~

--- app/models/concerns/rails_shop/product_search_methods.rb
+++ app/models/concerns/rails_shop/product_search_methods.rb
@@ -1,29 +1,31 @@
 # include ::RailsShop::ProductSearchMethods
 module RailsShop
   module ProductSearchMethods
-    # FULL TEXT SEARCH HELPERS
+    extend ActiveSupport::Concern
 
-    # Product.first.fts_data_rebuild!
-    # FULL TEXT SEARCH by title
-    def fts_data_rebuild!
-      fts_data = shop_categories.map(&:title) | shop_brands.map(&:title) | [title]
-      fts_data = fts_data.join ', '
+    class_methods do
+      # Product.first.fts_data_rebuild!
+      # FULL TEXT SEARCH by title
+      def fts_data_rebuild!
+        fts_data = shop_categories.map(&:title) | shop_brands.map(&:title) | [title]
+        fts_data = fts_data.join ', '
 
-      self.update_column :fts_auto_data, fts_data
-    end
-
-    # ::Product.rebuild_search_index!
-    def self.rebuild_search_index!
-      count = ::Product.count
-      ::Product.all.each_with_index do |product, index|
-        ::ShopItemsSearch.update_index(product)
-        puts "#{ index }/#{ count }"
+        self.update_column :fts_auto_data, fts_data
       end
-    end
 
-    # Product.fts_data_rebuild!
-    def self.fts_data_rebuild!
-      ::Product.all.each { |pr| pr.fts_data_rebuild! }
+      # ::Product.rebuild_search_index!
+      def rebuild_search_index!
+        count = ::Product.count
+        ::Product.all.each_with_index do |product, index|
+          ::ShopItemsSearch.update(product)
+          puts "#{ index }/#{ count }"
+        end
+      end
+
+      # Product.fts_data_rebuild!
+      def fts_data_rebuild!
+        ::Product.all.each { |pr| pr.fts_data_rebuild! }
+      end
     end
   end
 end
